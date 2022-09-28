2ec04d66dd48a3b7cc7f4588c91915551404e2bf
Jelena Djurdjevic <+>
2022-09-28 08:17:33 -0600

    commit tree 87d69a9cc65ad3e1366d472a483ae757a307b407

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    let data = {
+        name: name,
+        email: email
+    };
+
+    let handleData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch("http://localhost:3000/users", handleData)
+        .then(function(res) {
+          return res.json();
+        })
+        .then(function(object) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+}
